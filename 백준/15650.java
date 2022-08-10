1210276117ce23f1e06d339452a8f623d21ab081
jahun00 <67998815+jahun00@users.noreply.github.com>
2022-08-10 09:36:51 +0900

    Create 15650.java

---
+++ 백준/15650.java
@@ -0,0 +1,47 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj15650 {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+
+        arr = new int[M + 1];
+        visit = new boolean[N + 1];
+
+        permutation(N, M, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void permutation(int n, int m, int idx) {
+        if(idx == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(visit[i]) continue;
+            if(idx != 0 && arr[idx-1] >= i) continue;
+            visit[i] = true;
+            arr[idx] = i;
+            permutation(n, m, idx + 1);
+            visit[i] = false;
+        }
+    }
+}
