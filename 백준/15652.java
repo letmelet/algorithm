86f887dfa6cc2f733821b4576476a1ba3cad5ffc
jahun00 <67998815+jahun00@users.noreply.github.com>
2022-08-12 09:07:32 +0900

    Create 15652.java

---
+++ 백준/15652.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj15652 {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+
+        arr = new int[M + 1];
+
+        permutation(N, M, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void permutation(int n, int m, int idx) {
+        if(idx == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(idx > 0 && arr[idx-1] > i) continue;
+
+            arr[idx] = i;
+            permutation(n, m, idx + 1);
+        }
+    }
+}
