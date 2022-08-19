706f99bd66b69abd158dd75562a8682e935549e7
jahun00 <67998815+jahun00@users.noreply.github.com>
2022-08-19 09:25:28 +0900

    Create 15657.java

---
+++ 백준/15657.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class bj15657 {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static boolean[] visit;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        list = new ArrayList<>();
+        arr = new int[M + 1];
+        visit = new boolean[N + 1];
+
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.valueOf(st.nextToken()));
+        }
+        Collections.sort(list);
+
+        permutation(N, M, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void permutation(int n, int m, int idx) {
+        if(idx == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(idx > 0 && arr[idx - 1] > list.get(i)) continue;
+            arr[idx] = list.get(i);
+            permutation(n, m, idx + 1);
+        }
+    }
+}
