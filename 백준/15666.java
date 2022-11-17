aeb0da3a628dcad5fed5847992f5278985d775d0
jahun00 <67998815+jahun00@users.noreply.github.com>
2022-11-17 09:55:44 +0900

    Update 15666.java

--- 백준/15666.java
+++ 백준/15666.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class bj15666 {
+public class bj15666 { 
 
     static StringBuilder sb = new StringBuilder();
     static int[] arr;
